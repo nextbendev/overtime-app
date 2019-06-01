98f3ee53337516adfd7cf5048742f5c01de56658
nextbendev <gastronomicllc@gmail.com>
2019-06-01 14:33:13 -0400

    User create w/TDD

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creation" do
+
+    it "can be created" do
+      user = Users.create(email: "test@test.com", password: "gogo", password_confirmation: "gogo", first_name: "jon", last_name: "Snow")
+    end
+
+    it "can be created" do
+
+      expect(user).to be_valid
+    end
+
+    it "cannot be creaated without first_name, last_name," do
+      @user.first_name = nil
+      @user.last_name = nil
+      expect(@user).to_not be_valid 
+
+
+
+    end
+  end
 end
