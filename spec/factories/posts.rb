1b0f8a89aafd7186cbbf206bafd920198587fd2a
nextbendev <gastronomicllc@gmail.com>
2019-06-01 15:47:56 -0400

    Post & test setup

---
+++ spec/factories/posts.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :post do
+    date "2019-06-01"
+    rationale "MyText"
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "creation" do
-
-    it "can be created" do
-      user = Users.create(email: "test@test.com", password: "gogo", password_confirmation: "gogo", first_name: "jon", last_name: "Snow")
+    before do
+      @user = User.create(email: "test@test.com", password: "gogogo", password_confirmation: "gogogo", first_name: "Jon", last_name: "Snow")
     end
 
     it "can be created" do
-
-      expect(user).to be_valid
+      expect(@user).to be_valid
     end
 
-    it "cannot be creaated without first_name, last_name," do
+    it "cannot be created without first_name, last_name," do
       @user.first_name = nil
       @user.last_name = nil
-      expect(@user).to_not be_valid 
+      expect(@user).to_not be_valid
 
 
 
